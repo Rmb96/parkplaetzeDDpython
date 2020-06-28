aec58bb045fe031d69e508210c2bcd829a01bb27
radeberger2 <robert-busch@posteo.de>
2020-06-28 15:31:17 +0200

    fix v2

--- Parkplatz.py
+++ Parkplatz.py
@@ -1,7 +1,8 @@
 import requests
-import lxml.html as html
+import lxml.html
 from pprint import pprint
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
+import pandas as pd
 
 class Parkplatz:
     def __init__(self, url = 'https://www.dresden.de/apps_ext/ParkplatzApp/index'):
@@ -11,15 +12,11 @@ class Parkplatz:
         if resp.status_code != 200:
             # This means something went wrong.
             raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-        return resp.content
+        #print(resp.content) print the full site
+        return resp
 
     def gettablev1(self):
-        resp = requests.get(self.url)
-        if resp.status_code != 200:
-            # This means something went wrong.
-            raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-        #print(resp.content) print the full site
-        doc = html.fromstring(resp.content) #parse html
+        doc = lxml.html.fromstring(self.getdata().content) #parse html
         tr_elements = doc.xpath('//tr') # find all tables
         #pprint(tr_elements[1].text_content())
         #pprint(tr_elements[2].text_content())
@@ -38,22 +35,26 @@ class Parkplatz:
                 #pprint(tmp)
                 resultdata.append([tmp[0],tmp[2],tmp[4]])
         return(resultdata)
-            # example  p1 = Parkplatz()  pprint(p1.getdata())
+            
     def gettablev2(self):
-        return  self.get_tables(self.getdata())
+        return  self.makelist(self.getdata())
 
-    def makelist(self,table):
-        result = []
-        allrows = table.findAll('tr')
-        for row in allrows:
-            result.append([])
-            allcols = row.findAll('td')
-            for col in allcols:
-                thestrings = [unicode(s) for s in col.findAll(text=True)]
-                thetext = ''.join(thestrings)
-                result[-1].append(thetext)
-        return result
-
-    def get_tables(self, htmldoc):
-        soup = BeautifulSoup(htmldoc)
-        return soup.findAll('table')
+    def makelist(self,html):
+        df_list = pd.read_html(html.text) # this parses all the tables in webpages to a list
+        for i in range(1,len(df_list)):
+            df_list[i].columns = ['id','Ort', 'Stellplätze', 'frei']
+            df_list[i].drop(columns=['id'])
+            #pprint(df_list[i]["Stellplätze"])
+            #pprint(df_list[i])
+        df = df_list[1]
+        mydfs=[df]
+        for i in range(2,len(df_list)):
+            mydfs.append(df_list[i])
+        df = pd.concat(mydfs)
+        df.pop('id') # remove id line
+        df['Stellplätze'] = df['Stellplätze'].str.replace('Stellplätze', '')
+        df['frei'] = df['frei'].str.replace('frei', '')
+        df['Stellplätze'] = pd.to_numeric(df['Stellplätze'])
+        df['frei'] = pd.to_numeric(df['frei'])
+        #df = pd.concat(df_list[1])
+        return df;
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from Parkplatz import Parkplatz
 from pprint import pprint
 
 p1 = Parkplatz()
-#pprint(p1.gettablev1())
+pprint(p1.gettablev1())
 pprint(p1.gettablev2())

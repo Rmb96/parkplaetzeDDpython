98c4fa9606d7601326d369fbb74b24408f67c5b0
radeberger2 <robert-busch@posteo.de>
2020-06-29 21:53:48 +0200

    remove version 1

--- Parkplatz.py
+++ Parkplatz.py
@@ -1,7 +1,5 @@
 import requests
-import lxml.html
 from pprint import pprint
-#from bs4 import BeautifulSoup
 import pandas as pd
 
 class Parkplatz:
@@ -15,28 +13,7 @@ class Parkplatz:
         #print(resp.content) print the full site
         return resp
 
-    def gettablev1(self):
-        doc = lxml.html.fromstring(self.getdata().content) #parse html
-        tr_elements = doc.xpath('//tr') # find all tables
-        #pprint(tr_elements[1].text_content())
-        #pprint(tr_elements[2].text_content())
-        #pprint(dir(tr_elements[2]))
-        #pprint(tr_elements[2].classes)
-        resultdata = []
-        for i in range(1, len(tr_elements)):
-            tmp = tr_elements[i].text_content()
-            tmp = tmp.replace(" \n", "")
-            tmp = tmp.replace("  ", "")
-            tmp = tmp.split("\n")
-            del tmp[0]
-            del tmp[(len(tmp)-1)]
-            if len(tmp) > 4:
-                #print(str(len(tmp))+":"+str(i))
-                #pprint(tmp)
-                resultdata.append([tmp[0],tmp[2],tmp[4]])
-        return(resultdata)
-            
-    def gettablev2(self):
+    def gettable(self):
         return  self.makelist(self.getdata())
 
     def makelist(self,html):
